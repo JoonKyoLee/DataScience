ad8873cfb312176398f1af36ee196571ca336e61
JoonKyo <joonkyolee@gmail.com>
2024-05-29 01:03:23 +0900

    스케일링부분 수정

--- inspection_scaling.py
+++ inspection_scaling.py
@@ -6,11 +6,14 @@ from tabulate import tabulate
 import seaborn as sns
 
 data = pd.read_csv('merged_data/data_after_encoding.csv')
+#data = pd.read_csv('merged_data/data_after_remove_dirty_data.csv')
 
 # 데이터 프레임에 있는 모든 column
 columns = data.columns.values.tolist()
 
 df = data.copy()
+
+#print(tabulate(df[df['역명'] == '강남'], headers='keys', tablefmt='pretty'))
 
 # Scaling할 때 사용하지 않을 column 선정
 columns_dropped = ['사용일자', '노선명', '역명', '행정구역_경기남부', '행정구역_경기동부',
@@ -28,7 +31,7 @@ def process_rain_or_snow(df, column):
     non_zero_days[column] = np.log1p(non_zero_days[column])
 
     # RobustScaler 적용
-    scaler = RobustScaler()
+    scaler = MinMaxScaler()
     non_zero_days[column] = scaler.fit_transform(non_zero_days[[column]])
 
     # 비나 적설량이 있는 날과 없는 날 합치기
@@ -42,6 +45,7 @@ df[['총이용승객수']] = df[['총이용승객수']].applymap(lambda x: np.lo
 # 강수량과 적설량 처리
 df = process_rain_or_snow(df, '강수량')
 df = process_rain_or_snow(df, '적설')
+print(df['강수량'].describe())
 print(df)
 
 print(tabulate(df.head(5), headers='keys', tablefmt='pretty'))
@@ -75,7 +79,7 @@ df_robust = robust_scaler.fit_transform(df[columns_after_drop])
 df_robust = pd.DataFrame(df_robust, columns=columns_after_drop)
 
 # 스케일하지 않은 데이터와 결합
-df_robust_scaled = pd.concat([df_not_scaled, df_robust], axis=1)
+df_scaled = pd.concat([df_not_scaled, df_robust], axis=1)
 
 """
 # Min-max Scaler 이용
@@ -99,9 +103,9 @@ df_minmax_scaled = pd.concat([df_not_scaled, df_minmax], axis=1)
 #df_standard_scaled.to_csv('merged_data/standard_scaled.csv', index=False)
 #df_robust_scaled.to_csv('merged_data/data_after_scaling.csv')
 
-print(tabulate(df_robust_scaled[df_robust_scaled['사용일자'] == 20231026], headers='keys', tablefmt='pretty'))
+print(tabulate(df_scaled[df_scaled['사용일자'] == 20231026], headers='keys', tablefmt='pretty'))
 
-print(df_robust_scaled)
+print(df_scaled)
 """
 # 스케일러 인스턴스 생성
 scalers = {
@@ -127,5 +131,34 @@ plt.tight_layout()
 plt.show()
 """
 
-df_robust_scaled.to_csv('merged_data/data_after_scaling.csv')
+df_scaled.to_csv('merged_data/data_after_scaling.csv')
 
+# 히스토그램과 KDE 플롯
+def plot_distributions(df, columns, title):
+    plt.figure(figsize=(14, 7))
+    for col in columns:
+        sns.histplot(df[col], kde=True, label=col, bins=30)
+    plt.legend()
+    plt.title(title)
+    plt.show()
+
+# 강수량, 적설량, 총이용승객수의 분포 시각화
+plot_distributions(df_scaled, ['강수량', '적설', '총이용승객수'], '강수량, 적설량, 총이용승객수 분포')
+
+# 특정 컬럼의 분포 시각화 예시 (강수량)
+plt.figure(figsize=(12, 6))
+sns.histplot(df_scaled['강수량'], kde=True)
+plt.title('강수량 분포')
+plt.show()
+
+# 특정 컬럼의 분포 시각화 예시 (적설량)
+plt.figure(figsize=(12, 6))
+sns.histplot(df_scaled['적설'], kde=True)
+plt.title('적설량 분포')
+plt.show()
+
+# 특정 컬럼의 분포 시각화 예시 (총이용승객수)
+plt.figure(figsize=(12, 6))
+sns.histplot(df_scaled['총이용승객수'], kde=True)
+plt.title('총이용승객수 분포')
+plt.show()
